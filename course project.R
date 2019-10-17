bd4246a1a9584f52a5ee9ea5432929d222b90376
Evant33 <evanthompson438@gmail.com>
2019-10-16 20:22:17 -0700

    Final script

--- course project.R
+++ course project.R
@@ -7,46 +7,37 @@ set.seed(1)
 
 #import
 pml_training <- read.csv("~/Coursera Data Science Specialization/Practical Machine Learning/Coursera-Practical-Machine-Learning/pml-training.csv")
-validation <- read.csv("~/Coursera Data Science Specialization/Practical Machine Learning/Coursera-Practical-Machine-Learning/pml-testing.csv")
+validation <- read.csv("~/Coursera Data Science Specialization/Practical Machine Learning/pml-testing.csv")
 
 
 #partition
 inTrain <- createDataPartition(y=pml_training$classe, p=0.6, list = FALSE)
 training <- pml_training[inTrain,]
 testing <- pml_training[-inTrain,]
-dim(training)
-dim(testing)
 
 
 #preprocess
-preObj <- preProcess(training, method = c("center", "scale", "knnImpute")) 
-#subtract mean of each var and divide by standard dev
-#also can try method = "BoxCox", which takes continuous data and attempts to fit it to a normal distribution
-#knnImpute to deal with missing values. Find nearest neighbors and make a guess at the missing value
-train <- predict(preObj, training)
-test <- predict(preObj, testing)
-val <- predict(preObj, validation)
-
-#remove low var variables
-nzv <- nearZeroVar(train, saveMetrics = TRUE)
-train <- train[, which(nzv$nzv==FALSE)]
+training <- training[ ,colSums(is.na(training)) < nrow(training)*0.95]#remove cols with many NAs
+training <- training[,-c(1:5)]#do not predict on name or observations number
+testing <- testing[,-c(1:5)]#do not predict on name or observations number
+validation <- validation[,-c(1:5,160)]#do not predict on name or observations number
+nzv <- nearZeroVar(training, saveMetrics = TRUE)#remove low variance predictors
+training <- training[, which(nzv$nzv==FALSE)]
 
 
 #model (random forest)
 modFit_rf <- train(classe~., 
-                   method = "rf", 
+                   method = "rf",
                    trControl = trainControl(method = "cv", number = 3), 
                    metric = "Accuracy",
-                   data = train)
-
-print(modFit_rf) # Accuracy (in-sample) = .9998302
-
-pred_rf <- predict(modFit_rf, test)
-
-confusionMatrix(pred_rf, test$classe) # Accuracy (out-of-sample) = .9997
+                   data = training)
+print(modFit_rf) # Accuracy (in-sample) = 0.9942
+pred_rf <- predict(modFit_rf, testing)
+confusionMatrix(pred_rf, testing$classe) # Accuracy (out-of-sample) = 0.9978
 
 
-
-
+#apply model to validation set
+val_predictions <- rbind(validation$problem_id, as.character(predict(modFit_rf, validation)))
+val_predictions
 
 
